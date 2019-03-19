7ed4d61b12691cc4b498f31ce2fc72366f2218be
Chris Camacho <cacamacho91@gmail.com>
2019-03-19 10:41:14 +0000

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import { directors } from '../data';
+import React from 'react'
+import { directors } from '../data'
 
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map(director => {
+        return (
+          <div key={director.name}>
+            <h3>Name: {director.name}</h3>
+            <p>Movies:</p>
+            <ul>
+              {director.movies.map((movie, i) => (
+                <li key={i}>{movie}</li>
+              ))}
+            </ul>
+          </div>
+        )
+      })}
     </div>
-  );
+  )
 }
 
 export default Directors
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import { movies } from '../data';
+import React from 'react'
+import { movies } from '../data'
 
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map(movie => {
+        return (
+          <div key={movie.title}>
+            <h3>{movie.title}</h3>
+            <p>Time: {movie.time}</p>
+            <p>Genres:</p>
+            <ul>
+              {movie.genres.map((genre, i) => (
+                <li key={i}>{genre}</li>
+              ))}
+            </ul>
+          </div>
+        )
+      })}
     </div>
-  );
-};
+  )
+}
 
-export default Movies;
+export default Movies
